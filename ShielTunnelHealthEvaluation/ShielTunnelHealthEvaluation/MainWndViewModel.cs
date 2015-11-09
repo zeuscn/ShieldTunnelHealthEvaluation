b40eac03498b6d40b00dfa8aea33c198651ed2b7
zeuscn <528136130@qq.com>
2015-11-09 09:17:44 +0800

    is3数据获取

---
+++ ShielTunnelHealthEvaluation/ShielTunnelHealthEvaluation/MainWndViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IS3.Core;
+
+namespace ShielTunnelHealthEvaluation
+{
+    
+    class MainWndViewModel
+    {
+        DGObjectsCollection _monitorGroup;
+        public MainWndViewModel()
+        {
+            //inputProData();
+        }
+        /// <summary>
+        /// acquire data from iS3
+        /// </summary>
+        public void inputProData()
+        {
+            IMainFrame _mainFrame = Globals.mainframe;
+            Project _project = Globals.project;
+            Domain _monitorDom = _project.getDomain(DomainType.Monitoring);
+            _monitorGroup=_monitorDom.getObjects("MonitorGroup");
+        }
+    }
+}
