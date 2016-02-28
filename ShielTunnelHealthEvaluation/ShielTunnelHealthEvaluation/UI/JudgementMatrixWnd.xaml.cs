9886d39af92ad7946c420738643f11d74d47ba1f
ChenNan <528136130@qq.com>
2016-02-28 15:14:54 +0800

    column居中

--- ShielTunnelHealthEvaluation/ShielTunnelHealthEvaluation/UI/JudgementMatrixWnd.xaml.cs
+++ ShielTunnelHealthEvaluation/ShielTunnelHealthEvaluation/UI/JudgementMatrixWnd.xaml.cs
@@ -32,10 +32,13 @@ namespace ShieldTunnelHealthEvaluation.UI
         Dictionary<string, JudgementMatrixInfo> judgeMatrixDic;
         int matrixNo;
         int matrixTotalNo;
+        public List<string> MatrixGrade { get; set; }
         public List<string> Sequences{get;set;}
         DenseMatrix weighMatrix;
+        bool isFirstAfterContextChanged = true;
         public JudgementMatrixWnd(AHPIndexHierarchy ahpIndexHierarchy)
         {
+            MatrixGrade = new List<string> { "1/2", "1", "2"};
             InitializeComponent();
             _judgementMatrixInfosSet = new AllExpertJudgementMatrixs();
             judgemetnMatrixInfos = new JudgementMatrixsSetting(ahpIndexHierarchy);
@@ -44,7 +47,7 @@ namespace ShieldTunnelHealthEvaluation.UI
             matrixTotalNo = judgeMatrixDic.Count;
             JudgementMatrixInfo judgemtInfo = judgeMatrixDic.ElementAt(matrixNo).Value;
             RefreshData(judgemtInfo.IndexsSequence, judgemtInfo.JudgementMatrix);
-            
+            dgWeight.CellEditEnding += dgWeight_CellEditEnding;
         }
         public void RefreshData(List<string> sequences, DenseMatrix weighMatrix)
         {
@@ -65,7 +68,7 @@ namespace ShieldTunnelHealthEvaluation.UI
         {
             DataTable dependedDT=CreateDT(Sequences,weighMatrix);
             this.dgWeight.ItemsSource = dependedDT.DefaultView;
-            //this.dgWeight.DataContext = dependedDT;
+            isFirstAfterContextChanged = true;
         }
         private DataTable CreateDT(List<string> headers,DenseMatrix dm)
         {
@@ -82,8 +85,12 @@ namespace ShieldTunnelHealthEvaluation.UI
                     if(i==j)
                     {
                         dr[j] = 1;
+                        continue;
                     }
-                    dr[j] = dm[i, j];
+                    if (dm[i, j]==0)
+                    {
+                        dr[j] = 1;
+                    }
                 }
                 resultDT.Rows.Add(dr);
             }
@@ -97,7 +104,7 @@ namespace ShieldTunnelHealthEvaluation.UI
                 for (int j = 0; j < Sequences.Count; j++)
                 {
                     var currentRow=dgWeight.Items[i] as DataRowView;
-                    var cellContent = currentRow[i].ToString();
+                    var cellContent = currentRow[j].ToString();
                     if (i == j)
                     {
                         weighMatrix[i, j] = 1;
@@ -122,55 +129,104 @@ namespace ShieldTunnelHealthEvaluation.UI
                 return;
             }
             matrixNo++;
+            JudgementMatrixInfo judgemtInfo = judgeMatrixDic.ElementAt(matrixNo).Value;
+            NextMatrix:
             if (matrixNo < matrixTotalNo)
             {
-                JudgementMatrixInfo judgemtInfo = judgeMatrixDic.ElementAt(matrixNo).Value;
-                if (judgemtInfo.IndexsSequence.Count== 1) //一个元素的
+                if (judgemtInfo.IndexsSequence.Count== 1) //一个元素的，跳过，并赋值
                 {
                     judgeMatrixDic.ElementAt(matrixNo).Value.JudgementMatrix = new DenseMatrix(1,1,new double[]{1});
                     judgeMatrixDic.ElementAt(matrixNo).Value.CalculateEigenVector();
-                    matrixNo++;
+                    matrixNo++;//接下来继续
+                    goto NextMatrix;
                 }
-                RefreshData(judgemtInfo.IndexsSequence, judgemtInfo.JudgementMatrix);
             }
-            if (matrixNo == matrixTotalNo - 1)
+            //不为一个元素,切实最后一个
+            if (matrixNo == matrixTotalNo - 1)//如果是最后一个
             {
                 this.btnOK.Content = "完成";
+                judgemtInfo = judgeMatrixDic.ElementAt(matrixNo).Value;
+                RefreshData(judgemtInfo.IndexsSequence, judgemtInfo.JudgementMatrix);
+                return;
             }
-            if (matrixNo >= matrixTotalNo)
+            //没了
+            if (matrixNo >= matrixTotalNo)//如果没了
             {
                 _judgementMatrixInfosSet.JudgementMatrixInfosList.Add(judgemetnMatrixInfos);
                 BinaryIO.OutputMatrixInfosSet(_judgementMatrixInfosSet);
                 this.Close();
                 return;
             }
+            //还没结束，也不是最后一个，也不是一个元素
+            judgemtInfo = judgeMatrixDic.ElementAt(matrixNo).Value;
+            RefreshData(judgemtInfo.IndexsSequence, judgemtInfo.JudgementMatrix);
         }
 
         private void dgWeight_LoadingRow(object sender, DataGridRowEventArgs e)
         {
             e.Row.Header = this.Sequences[e.Row.GetIndex()];
+            e.Row.HorizontalContentAlignment = HorizontalAlignment.Center;
+            e.Row.HorizontalAlignment = HorizontalAlignment.Center;
         }
 
         private void dgWeight_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
             DataGridComboBoxColumn comboBoxColumn = new DataGridComboBoxColumn();
             comboBoxColumn.Header = e.PropertyName;
-            // ComboBox 选择后的值
-            comboBoxColumn.SelectedValuePath = e.PropertyName;
-            // ComboBox 前台显示的值
-            comboBoxColumn.DisplayMemberPath = e.PropertyName;
 
             Binding binding = new Binding();
             // 需要绑定的列
             binding.Path = new PropertyPath(e.PropertyName);
             binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
             comboBoxColumn.SelectedValueBinding = binding;
-
             Binding itemsSourceBinding = new Binding();
-            itemsSourceBinding.Source = new List<string>{"1/2","1","2","0"};
+            itemsSourceBinding.Source = this;
+            itemsSourceBinding.Path = new PropertyPath("MatrixGrade");
             BindingOperations.SetBinding(comboBoxColumn, DataGridComboBoxColumn.ItemsSourceProperty, itemsSourceBinding);
-
+            //居中样式
+            Style styleCenter = new Style(typeof(ComboBox));
+            Setter setHorizonalCenter = new Setter(ComboBox.HorizontalContentAlignmentProperty, HorizontalAlignment.Center);
+            Setter setVerticalCenter = new Setter(ComboBox.VerticalContentAlignmentProperty, VerticalAlignment.Center);
+            styleCenter.Setters.Add(setHorizonalCenter);
+            styleCenter.Setters.Add(setVerticalCenter);
+            comboBoxColumn.ElementStyle = styleCenter;
+            comboBoxColumn.EditingElementStyle = styleCenter;
+            //Style styleCenter2 = new Style(typeof(ComboBox));
+            //Setter setHorizonalCenter2 = new Setter(ComboBox., HorizontalAlignment.Center);
+            //Setter setVerticalCenter2 = new Setter(ComboBox.VerticalAlignmentProperty, VerticalAlignment.Center);
             e.Column = comboBoxColumn;
+
+           
+        }
+        private void dgWeight_CellEditEnding(object sender,DataGridCellEditEndingEventArgs e)
+        {
+            var originRowHeader = e.Row.Header.ToString();
+            var originColHeader = e.Column.Header.ToString();
+            int originRowIndex = this.Sequences.IndexOf(originRowHeader);
+            int originColIndex = this.Sequences.IndexOf(originColHeader);
+            var originCell = this.dgWeight.GetCell(originRowIndex, originColIndex);
+            var originComBox = (ComboBox)originCell.Content;
+            var originSeletectedIndex = originComBox.SelectedIndex;
+            var toModifyCell=this.dgWeight.GetCell(originColIndex, originRowIndex);
+            var toModifyCmBox=(ComboBox) toModifyCell.Content;
+            toModifyCmBox.SelectedIndex = this.MatrixGrade.Count-1-originSeletectedIndex;
+        }
+        private void SetDiagonalUnediteable()
+        {
+            for(int i=0;i<this.Sequences.Count;i++)
+            {
+                var diagonalCell=this.dgWeight.GetCell(i, i);
+                diagonalCell.IsEnabled = false;
+            }
+        }
+
+        private void dgWeight_LayoutUpdated(object sender, EventArgs e)
+        {
+            if(isFirstAfterContextChanged)
+            {
+                SetDiagonalUnediteable();
+                isFirstAfterContextChanged = false;
+            }
         }
     }
 }
